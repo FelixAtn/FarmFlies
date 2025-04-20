0975fc4ab5741c0ee2d2dcaa198319cf03ea4b14
Felix Atanasescu <felix.att@protonmail.com>
2025-04-20 19:33:29 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "stdafx.h"
 #include "GameInstance.h"
 
+#ifdef _DEBUG
+// Debug mode: show console
 int main()
 {
 	GameInstance instance;
 	instance.Run();
 	return 0;
 }
+#else
+// Release mode: hide console
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	GameInstance instance;
+	instance.Run();
+	return 0;
+}
+#endif
+
